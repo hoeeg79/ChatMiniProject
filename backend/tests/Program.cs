dce61376d257aa8cdbcf6d689421a6f6536b82af
hoeeg79 <93433669+hoeeg79@users.noreply.github.com>
2024-02-21 09:39:48 +0100

    naaaasstyyy

--- backend/tests/Program.cs
+++ backend/tests/Program.cs
@@ -1,20 +1,32 @@
 ﻿using lib;
 using NUnit.Framework;
+using api.ClientEventHandlers;
+using tests;
 
 [TestFixture]
 public class Test
 {
+    [SetUp]
+    public void SetupThatShit()
+    {
+        HelperBitch.TriggerRebuild();
+    }
+    
     [Test]
     public async Task MyTest()
     {
         //Initialize the WebSocketTestClient and connect to the server (default URL = ws://localhost:8181)
         var ws = await new WebSocketTestClient().ConnectAsync();
+
+        await ws.DoAndAssert(new ClientWantsToSignInDto() { Username = "Dorit"});
+
+        await ws.DoAndAssert(new ClientWantsToEnterRoomDto() { RoomId = 1 });
         
         //Send an object extending BaseDto to the server without asserting and waiting
-        await ws.DoAndAssert(new ClientWantsToEchoDto() {message = "hey"});
+        await ws.DoAndAssert(new ClientWantsToBroadcastToRoomDto() {Message = "hey", RoomId = 1});
    
         //Send an object extending BaseDto to the server and wait for assertions to be true. If not, exception is thrown
-        await ws.DoAndAssert(new ClientWantsToEchoDto() { message = "hey"}, 
-            receivedMessages => receivedMessages.Count == 2);
+        await ws.DoAndAssert(new ClientWantsToBroadcastToRoomDto() {Message = "hey2", RoomId = 1}, 
+            receivedMessages => receivedMessages.Count == 1);
     }
 }
